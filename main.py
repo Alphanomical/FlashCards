c0fad4b277ca6845fc643598c40620226d3f8532
Amit Anand <amit1011100@gmail.com>
2019-01-12 17:40:46 -0800

    Added set change functionality. Error handling for missing sets.

--- main.py
+++ main.py
@@ -14,21 +14,22 @@ import codecs
 from random import choice
 from timeit import default_timer as timer
 import tkinter as tk
+from tkinter import messagebox
 
-fileName="testset.txt"
+
 
 class FlashCardMain(object):
     pairs={}
     frt,bck='',''
     cardSide=True
-    packs={"testset.txt","testset2.txt"}
-
+    packs={"testset","testset2","error_trigger"}
+    fileName = "testset.txt"
     def __init__(self, parent, *args, **kwargs):
         self.root=parent
         self.make={}
         parent.title("Flash Card")
         self.loadSet()
-        self.nextCard()
+        self.frt, self.bck = self.serveCard()
         self.tkvar = tk.StringVar()
         self.tkvar.set("testset.txt")
 
@@ -38,26 +39,32 @@ class FlashCardMain(object):
         popupMenu.place(x=175,y=10)
         self.tkvar.trace('w', self.change_dropdown)
 
+        #Exit Button
         self.exitButton = tk.Button(parent, text="Exit", fg="red", command=quit)
         self.exitButton.place(x=10,y=10)
 
+        #The 'face' of the flash card
         self.flashData=tk.Label(parent,text=self.frt)
+        self.flashData.config(font=("courier",10))
         self.flashData.place(x=150,y=100)
 
         #note to self:
         #passing an arguement into nextFlashCard triggers the button press immediately
         #command takes a reference to a function, adding an arguement ends up calling the function
         #use lambda to create anonoymous function, cleanly fixes this problem
-        self.nextButton = tk.Button(parent, text="Flip Card",command=lambda:self.flipFlashCard(parent))
+        self.nextButton = tk.Button(parent, text="Flip Card",command=lambda:self.flipFlashCard())
         self.nextButton.place(x=10,y=100)
+
+        self.shuffleCard = tk.Button(parent, text="Random Card",command=lambda:self.changeCard())
+        self.shuffleCard.place(x=10, y=140)
 
     def change_dropdown(self,*args):
         print(self.tkvar.get())
+        self.fileName = self.tkvar.get()+".txt"
+        self.loadSet()
+        self.changeCard()
 
-    def nextCard(self):
-        self.frt, self.bck = self.serveCard()
-
-    def flipFlashCard(self,parent):
+    def flipFlashCard(self):
         print("button pressed")
         if(self.cardSide):
             self.flashData.configure(text=self.bck)
@@ -66,20 +73,29 @@ class FlashCardMain(object):
         self.cardSide=not self.cardSide
         self.flashData.place(x=150,y=100)
 
+    def changeCard(self):
+        self.frt,self.bck=self.serveCard()
+        self.flashData.configure(text=self.frt)
+        self.cardSide=True
+
     def serveCard(self):
         frontSide = choice(list(self.pairs))
         backSide = self.pairs[frontSide]
         return frontSide, backSide
 
     def loadSet(self):
+        print("loading set")
         try:
             # file=open(fileName,"r")
-            file = codecs.open(fileName, encoding='utf-8')
+            file = codecs.open(self.fileName, encoding='utf-8')
+            self.pairs.clear()
+            for i, line in enumerate(file):
+                eng, jpn = line.split(":")
+                self.pairs[eng] = jpn
         except:
             print("Error: opening file")
-        for i, line in enumerate(file):
-            eng, jpn = line.split(":")
-            self.pairs[eng] = jpn
+            messagebox.showerror("Error","File not found")
+
 
 
 if __name__ == "__main__":
